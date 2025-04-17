d584a587456e533c8391f0c9f0e388eca9088f1c
Mateo lizcano <mateodevcode@gmail.com>
2025-04-17 17:30:55 +0200

    Se cambio a prod

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Cambia esto por tu dominio en producción
+    origin: "https://bordex.vercel.app", // Cambia esto por tu dominio en producción
     methods: ["GET", "POST"],
   },
 });
